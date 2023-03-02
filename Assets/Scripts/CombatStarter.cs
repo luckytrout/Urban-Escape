e44c018935ce5855312679f2e1f3427419cfaca3
Lucas Troutman <100403421+luckytrout@users.noreply.github.com>
2023-03-01 20:18:36 -0800

    Adding battle camera & enemy NPC

---
+++ Assets/Scripts/CombatStarter.cs
@@ -0,0 +1,34 @@
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatStarter : MonoBehaviour
+{
+    [SerializeField] Camera battleCamera;
+    [SerializeField] Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other) {
+        if(other.transform.tag == "Player"){
+            Debug.Log("Player started combat in " + gameObject);
+            battleCamera.gameObject.SetActive(true);
+            mainCamera.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("Player exited combat" + gameObject);
+        mainCamera.gameObject.SetActive(true);
+        battleCamera.gameObject.SetActive(false);
+    }
+}
