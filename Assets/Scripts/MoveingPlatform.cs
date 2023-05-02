7da272e82aaf8973fea0a5ea71c4860e848a08ed
CsunTylerC <124757717+CsunTylerC@users.noreply.github.com>
2023-05-01 18:17:31 -0700

    moving platform update
    scripts work. Prefab to be made

--- Assets/Scripts/MoveingPlatform.cs
+++ Assets/Scripts/MoveingPlatform.cs
@@ -5,50 +5,58 @@ using UnityEngine;
 public class MoveingPlatform : MonoBehaviour
 {
     [SerializeField]
-    private Waypoints _waypoints;
+    private WaypointPath _waypointPath;
 
     [SerializeField]
     private float _speed;
 
-    private int _targetWaypointIndex;
+    private int _targetIndex;
+    private Transform _previous;
+    private Transform _target;
 
-    private Transform _PreviousWP;
-    private Transform _TargetWP;
-
-    private float _timeToWP;
-    private float _elapstedTime;
+    private float _timeto;
+    private float _elapsed;
 
     // Start is called before the first frame update
     void Start()
     {
-        TargetNextWP();
+        TargetNext();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-       _elapstedTime = +- Time.deltaTime; 
+        _elapsed += Time.deltaTime;
 
-       float elapstedPercent = _elapstedTime/_timeToWP;
-       elapstedPercent = Mathf.SmoothStep(0, 1, elapstedPercent);
-       transform.position = Vector3.Lerp(_PreviousWP.position, _TargetWP.position, elapstedPercent);
+        float elapstedPersent = _elapsed / _timeto;
+        elapstedPersent = Mathf.SmoothStep(0, 1, elapstedPersent);
+        transform.position = Vector3.Lerp(_previous.position, _target.position, elapstedPersent);
 
-       if(elapstedPercent >= 1) 
-       {
-        TargetNextWP();
-       }
+        if(elapstedPersent >= 1)
+        {
+            TargetNext();
+        }
     }
 
-    private void TargetNextWP() 
+    private void TargetNext()
     {
-        _PreviousWP = Waypoints.GetWaypoint(_targetWaypointIndex);
-        _targetWaypointIndex = Waypoints.GetNextWPI(_targetWaypointIndex);
-        _TargetWP = Waypoints.GetWaypoint(_targetWaypointIndex);
+        _previous = _waypointPath.GetWP(_targetIndex);
+        _targetIndex = _waypointPath.GetNextWP(_targetIndex);
+        _target = _waypointPath.GetWP(_targetIndex);
 
-        _elapstedTime = 0;
+        _elapsed = 0;
 
-        float distance = Vector3.Distance(_PreviousWP.position, _TargetWP.position);
-        _timeToWP = distance/ _speed;
+        float _distance = Vector3.Distance(_previous.position, _target.position);
+        _timeto = _distance/_speed;
+    }
 
+    private void OnTriggerEnter(Collider other) 
+    {
+        other.transform.SetParent(transform);
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        other.transform.SetParent(null);
     }
 }
